5f00648b4cf470d3b7e20cf96327fd9fcb3cc189
Risuban <nicotapiamunoz@gmail.com>
2023-05-10 20:55:38 -0400

    started with the button, basic AF, no login at all

--- medset/src/App.js
+++ medset/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Route } from "wouter";
 
 import Lista_medicos from './Components/Lista_medicos';
+import Inicio from './Components/Inicio';
 
 function App() {
   return (
     <div className="App">
           <Route component={Lista_medicos} path="/lista_medicos" />
+          <Route component={Inicio} path='/'/>
       
     </div>
   );
