c7cc3c0fcde93d79ca616c04fbd2d5e8b91901d8
Risuban <nicotapiamunoz@gmail.com>
2023-05-11 21:45:11 -0400

    FINALLY

--- medset/src/App.js
+++ medset/src/App.js
@@ -3,16 +3,16 @@ import { Route } from "wouter";
 
 
 import Home from './Components/Home';
-
 import Lista_medicos from "./Components/Lista_medicos";
 import Formulario_MedicoNuevo from "./Components/Formulario_NuevoMedico";
+import Editar_medico from "./Components/Editar_Medico";
 
 function App() {
   return (
     <div className="App">
           <Route component={Home} exact path="/"/>
           <Route component={Lista_medicos} path="/lista_medicos" />
-          <Route component={Formulario_MedicoNuevo} path="/formulario_medicos" />
+          <Route component={Formulario_MedicoNuevo} path="/formulario_medicos"/>
           <Route component={Editar_medico} path="/formulario_medicos/:keyword"/>
 
     </div>
