afa746ec3246859b0d43838a0645ee7bc5f8968a
pvvegar <129302057+pvvegar@users.noreply.github.com>
2023-07-04 15:40:14 -0400

    fixed some thigs on Selenium

--- medset/src/Components/Editar_Medico.js
+++ medset/src/Components/Editar_Medico.js
@@ -7,7 +7,7 @@ import { useLocation } from "wouter";
 export default function Editar_medico ( {params} ) {
     const { keyword } = params
     const [geographic_location, setGeographic_location] = useState("");
-    const [setLocation] = useLocation();
+    const [location, setLocation] = useLocation();
 
     useEffect(function () {
         getMedicoEspecifico( {keyword} ).then((ficha) => {
